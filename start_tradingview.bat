2364f9b3c5e01faa865e535235a71c3c58958708
Hoan <nguyentranhoan.mr@gmail.com>
2021-04-25 10:11:12 +0700

    create run_tradingView.bat

---
+++ start_tradingview.bat
@@ -0,0 +1,7 @@
+cd C:\Users\HP\Desktop\TradingView_Web
+
+set FLASK_APP=main.webapp:app
+
+C:\Users\HP\AppData\Local\Programs\Python\Python39\python.exe -m flask run &
+
+start chrome.exe http://127.0.0.1:5000/
