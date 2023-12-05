a444686e8585263e3ec36dd219ae2e89b56e7239
Hoan <nguyentranhoan.mr@gmail.com>
2023-12-05 21:16:35 +0700

    update from anh huy computer

--- main/playground.py
+++ main/playground.py
@@ -1,16 +1,4 @@
-import sqlite3
-import pandas as pd
-
-DATABASE_URL = "C:/Users/Nguye/Documents/GitHub/TradingView_Web/main/test.db" #'C:/Users/HP/Desktop/TradingView_Web/main/.data'
-
-if __name__ == '__main__':
-    conn = sqlite3.connect(DATABASE_URL)
-    # save sqlite table in a DataFrame
-    momentum = pd.read_sql('SELECT * from momentum', conn)
-    # write DataFrame to CSV file
-    momentum.to_csv('C:/Users/Nguye/Documents/GitHub/TradingView_Web/main/.data/momentum.csv', index=False)
-    
-
-    """
-    REMEMBER TO ADJUST momentum.csv file FORMAT BEFORE MIGRATING TO NEW DATABASE.
-    """
+from datetime import datetime
+date_time_str = "08 Jun 22 22:45"
+date_time = datetime.strptime(date_time_str, '%d %b %y %H:%M')
+print(date_time)
